2a9d95d8be92e9cb28772cbd28c43c620b3940a2
Amit998 <damitlucky998@gmail.com>
2020-07-19 16:57:33 +0530

    Removed The Current Value couter Problem

--- Digit _Pairs.py
+++ Digit _Pairs.py
@@ -1,4 +1,7 @@
 import math
+import time
+current=time.time()
+print(current)
 num=["8" ,"234", "567", "321","345", "123" ,"110", "767", "111"]
 lst=[]
 for i in range(len(num)):
@@ -34,7 +37,7 @@ for j in range(len(lst)):
         odd.append(val)
         flag=flag-1
         val=""
-def counter(even,odd):
+def counter(even,odd,current):
     count=0
     tempList=[]
     evenCounterList=[]
@@ -44,37 +47,64 @@ def counter(even,odd):
         tempList=list(even[i])
         significant=tempList[0]
         evenCounterList.append(significant)
-    tempList=[]
     for j in range(len(even)):
         tempList=list(odd[j])
         significant=tempList[0]
         oddCounterList.append(significant)
-    for k in range(len(evenCounterList)):
-        counterEven=evenCounterList.count(evenCounterList[k])
-        newNoDuplicate.append(counterEven)
-    mylist = list(dict.fromkeys(newNoDuplicate))
-    for i in range(len(mylist)):
-        if( mylist[i] % 2 == 0  ):
-            count=count+1
-        elif( mylist[i] > 0 and mylist[i] % 2 !=0 ):
+    tempList=[]
+   
 
-            count=int(math.ceil(mylist[i]/2))
+    for k in range(10):
+        counterEven=evenCounterList.count(str(k))
+        counterOdd=oddCounterList.count(str(k))
         
-    newNoDuplicate=[]
-    
-    
-    for l in range(len(oddCounterList)):
-        counterOdd=oddCounterList.count(oddCounterList[l])
-        newNoDuplicate.append(counterOdd)
 
-    mylist = list(dict.fromkeys(newNoDuplicate))
+        if( counterEven > 1 ):
+            tempList.append(counterEven)
+        elif( counterOdd > 1 ):
+            tempList.append(counterOdd)
+
+
+    for l in range(len(tempList)):
+    
+        if( tempList[l] % 2 == 0  ):
+            count=count+1
+          
+        elif( tempList[l] > 0 and tempList[l] % 2 !=0 ):
+            count=int(math.ceil(tempList[l]/2))+count
+            
+
+    # for k in range(len(evenCounterList)):
+    #     counterEven=evenCounterList.count(evenCounterList[k])
+    #     newNoDuplicate.append(counterEven)
   
-    if( mylist[i] % 2 == 0  ):
-            count=count+1
-    elif( mylist[i] > 0 and mylist[i] % 2 !=0 ):
-        count=int(math.ceil(mylist[i]/2))+count
+
+
+    # mylist = list(dict.fromkeys(newNoDuplicate))
+
+    # for i in range(len(mylist)):
+    #     if( mylist[i] % 2 == 0  ):
+    #         count=count+1
+    #     elif( mylist[i] > 0 and mylist[i] % 2 !=0 ):
+
+    #         count=int(math.ceil(mylist[i]/2))
         
-    return count
+    # newNoDuplicate=[]
+    
+    
+    # for l in range(len(oddCounterList)):
+    #     counterOdd=oddCounterList.count(oddCounterList[l])
+    #     newNoDuplicate.append(counterOdd)
+
+    # mylist = list(dict.fromkeys(newNoDuplicate))
+  
+    # if( mylist[i] % 2 == 0  ):
+    #         count=count+1
+    # elif( mylist[i] > 0 and mylist[i] % 2 !=0 ):
+    #     count=int(math.ceil(mylist[i]/2))+count
+        
+    return count,time.time()-current
    
     
-print(counter(even,odd))
+print(counter(even,odd,current))
+
