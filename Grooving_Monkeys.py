116fa663f77e8b0cf0d2c4134b59f253c75cf393
Amit Dutta <damitlucky998@gmail.com>
2020-07-22 19:41:02 +0530

    Add files via upload

---
+++ Grooving_Monkeys.py
@@ -0,0 +1,38 @@
+def g_monkey(unOrdered,length):
+    sec=0
+    n=0
+    Ordered=sorted(unOrdered)
+    check=Ordered
+    position=[]
+    for j in range(length):
+        for i in range(length):
+            if(Ordered[i] == unOrdered[j]):
+                # unOrdered[j]="FILLED"
+                position.append(i)
+                
+                break
+    print(position)
+    while( n != 1 ):
+        sec=sec+1
+        tempList=[]
+        for i in range(length):
+            tempList.append(Ordered[position[i]])
+
+        print(tempList,check)
+        if(tempList == check):
+            # n=1
+            # print('matched')
+            return sec
+        else:
+            Ordered=tempList
+            n=0
+for i in range(int(input())):
+    unOrdered=list(map(str,input().split()))
+    # unOrdered=['a','b','d','c']
+    # length=(int(input()))
+    length=len(unOrdered)
+    print(g_monkey(unOrdered,length))   
+
+# print(tempList)
+# a=2
+# b=5
