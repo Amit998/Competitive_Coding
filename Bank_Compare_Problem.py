1af33420925c3d58a6c14d404e1fa67ab80fadb9
Amit998 <damitlucky998@gmail.com>
2020-08-10 10:17:12 +0530

    Bank Compare Problem updated

--- Bank_Compare_Problem.py
+++ Bank_Compare_Problem.py
@@ -1,32 +1,37 @@
-# 5
-# 9.5
-# 10
-# 9.6
-# 5
-# 8.5
-# 10
-# 6.9
-# 5
-# 8.5
-# 5
-# 7.9
+
+# 500000
+# 26
+# 3
+# 13 9.5
+# 3 6.9
+# 10 5.6
+# 3
+# 14 8.5
+# 6 7.4
+# 6 9.6
+
+
 
 import math
-# Principle=int(input())
-# Time=int(input())
-Principle=10000
-Time=20
+Principle=int(input())
+Time=int(input())
+# Principle=10000
+# Time=20
 EMI=[]
-slab=3
+# slab=3
+
 sum=0
 for j in range(2):
+    slab=int(input())
     sum=0
-    for i in range(slab): 
-        NumberOfYear=int(input())
-        monthlyIRate=float(input())
-        A1=Principle * monthlyIRate
-        ToThePower=NumberOfYear*12
-        B1=1+monthlyIRate ** ToThePower
+    for i in range(slab):
+        NumberOfYear,monthlyIRate=input().split()
+        monthlyIRate=float(monthlyIRate)
+        # NumberOfYear=int(input())
+        # monthlyIRate=float(input())
+        A1=Principle * int(monthlyIRate)
+        ToThePower=int(NumberOfYear)*12
+        B1=1+int(monthlyIRate) ** ToThePower
         C1=1-1/B1
         D1=A1 /C1
         sum=D1+sum
