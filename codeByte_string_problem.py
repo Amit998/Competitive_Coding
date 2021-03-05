77425002f3ebbe5722504344ee695368401bcf8b
Amit Dutta <damitlucky998@gmail.com>
2021-03-05 08:37:41 +0530

    Create codeByte_string_problem.py

---
+++ codeByte_string_problem.py
@@ -0,0 +1,13 @@
+def string_challage(Test_string):
+        count = 0
+        Result = ""
+        for i in range(len(Test_string)):
+            if (i+1) < len(Test_string) and (Test_string[i] == Test_string[i+1]):
+                count += 1
+            else:
+                Result += str((count+1))+Test_string[i]
+                count = 0
+        return Result
+
+
+print(string_challage("aabcaaacddd"))
