79b8c28fd7dcf20891ecf9520e18c1393247f7cd
Amit Dutta <damitlucky998@gmail.com>
2021-11-25 21:09:41 +0530

    Create countAndSay.java

---
+++ countAndSay.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String countAndSay(int n) {
+        
+        if(n==1) return "1";
+        
+        String s=countAndSay(n-1);
+        
+        StringBuilder res=new StringBuilder();
+        
+        int counter=0;
+        
+        for(int i=0;i<s.length();i++){
+            counter++;
+            if(i==s.length()-1 || s.charAt(i)!=s.charAt(i+1)){
+                // res=res+counter+s.charAt(i);
+                res.append(counter).append(s.charAt(i));
+                counter=0;
+                
+            }
+        }
+        
+        return res.toString();
+        
+    }
+}
